43a0987bafa2de89dec5f998f23bdf1f96d90e6c
anishghimire <anishghimire862@gmail.com>
2019-08-22 22:56:28 +0545

    Lock file removed, changes in packages and attendance post request implemented

--- app/Controller/attendances.controller.js
+++ app/Controller/attendances.controller.js
@@ -6,10 +6,10 @@ module.exports = {
         let section = req.params.section;
         let semester = req.params.semester;
         db.query("SELECT students.id as 'student_id', student_semesters.id AS 'student_semester_id', "+
-            "name FROM "+
+            "name, image FROM "+
             "students "+
             "INNER JOIN student_semesters ON students.id = "+
-            "student_semesters.student_id AND NOW() BETWEEN `from` AND `TO` AND students.faculty=?"+
+            "student_semesters.student_id AND NOW() BETWEEN `from` AND `to` AND students.faculty=?"+
             "AND student_semesters.section =? and student_semesters.semester =?", [faculty, section, semester], 
             (err, data) => {
                 if(err)
@@ -28,7 +28,21 @@ module.exports = {
                     }
                 } 
             }
-        )
-        
+        )  
+    },
+    submitAttendance: function(req, res) {
+        let datetime = new Date()
+        let date = datetime.toISOString().slice(0,10)
+        let student_semester_id = req.student_semester_id
+        let status = req.status
+        let subject_code = req.subject_code
+
+        db.query("INSERT INTO attendance(date, student_semester_id, status, subject_code) values(?,?,?,?)",
+            [date, student_semester_id, status, subject_code], (err, data) => {
+                if(err)
+                    res.json(err)
+                else   
+                    res.status(201).json({ data: req.body });
+            })
     }
 }
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
-
+var serveStatic = require('serve-static');
 var app = express();
 
 var port = process.env.PORT || 8001;
 app.use(bodyParser.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
+app.use(serveStatic('public/images'))
 
 app.listen(port, function () {
 	console.log("Running on port " + port);
