8933d150a0dd0eed06b3c4da7b563ad573aca692
anishghimire <anishghimire862@gmail.com>
2019-11-27 22:28:31 +0545

    Removed attendance attendance dto file

--- app/Controller/attendanceDto.js
+++
@@ -1,7 +0,0 @@
-module.exports = class AttendanceDto {
-	constructor(status, subjectSemesterId, subjectCode) {
-		this.status = status
-		this.subjectSemesterId = subjectSemesterId,
-		this.subjectCode = subjectCode
-	}
-}
